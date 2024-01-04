48bab984e3e39709f1bd8e1e74c72677d8d01ed4
Yuksel <ykselbaltacioglu@gmail.com>
2024-01-04 23:11:57 +0300

    First setup

---
+++ GitHubFollower/FavoritesListVC.swift
@@ -0,0 +1,19 @@
+//
+//  FavoritesListVC.swift
+//  GithubFollower
+//
+//  Created by Yüksel Baltacıoğlu on 4.01.2024.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBlue
+    }
+    
+
+}
---
+++ GitHubFollower/SearchVC.swift
@@ -0,0 +1,19 @@
+//
+//  SearchVC.swift
+//  GithubFollower
+//
+//  Created by Yüksel Baltacıoğlu on 4.01.2024.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+    
+
+}
